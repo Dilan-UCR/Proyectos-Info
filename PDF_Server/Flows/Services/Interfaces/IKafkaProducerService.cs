ae5d2361a77681f187f8ceb89e8539cb09d07bb5
Dilan Cristian Flores Martine <dilan.flores@ucr.ac.cr>
2025-10-08 00:32:56 -0600

    Correlation a kafka listo

---
+++ PDF_Server/Flows/Services/Interfaces/IKafkaProducerService.cs
@@ -0,0 +1,9 @@
+﻿using PDF_Server.Flows.DTOs;
+
+namespace PDF_Server.Flows.Services.Interfaces
+{
+    public interface IKafkaProducerService
+    {
+        Task<bool> SendLogAsync(LogRequestDto log);
+    }
+}
